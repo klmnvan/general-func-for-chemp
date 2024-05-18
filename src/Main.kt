1ea593f7b9e945b6168172944e31eeaa479b5302
klmnvan <126891264+klmnvan@users.noreply.github.com>
2024-05-18 20:28:58 +0300

    add work for basket and products

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,58 @@
 import General.toSHA512
+import java.math.BigInteger
+import java.security.MessageDigest
 import java.sql.Timestamp
 import java.time.Duration
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Получение разницы между датами")
+
+    var listProduct = ArrayList<ProductModel>()
+    listProduct.add(ProductModel(true, "Тестовый заголовок 1", "Какое-то описание", 500, "Tennis"))
+    listProduct.add(ProductModel(false, "Тестовый 2",  "Какое-то описание",600, "Tennis"))
+    listProduct.add(ProductModel(true, "заголоsdfвок 3",  "Какое-то опsdfgисание",700, "Outdoor"))
+    listProduct.add(ProductModel(true, "заголоsdfвок 3",  "Какое-то опsdfgисание",700, "Outdoor"))
+    listProduct.add(ProductModel(true, "заголоsdfвок 3",  "Какое-то опsdfgисание",700, "Outdoor"))
+    listProduct.add(ProductModel(true, "заголsdfовок 4",  "Какsdfое-sdfто описание",800, "Tennis"))
+
+    println("Получить сумму корзины: ${listProduct.map { it.price!! }.sum()}")
+    val distinctProduct = listProduct.distinct()
+    distinctProduct.forEach { el ->
+        println("${el.name}: ${listProduct.count { it == el }}")
+    }
+    listProduct.removeAll { it == (ProductModel(true, "заголоsdfвок 3",  "Какое-то опsdfgисание",700, "Outdoor")) }
+    distinctProduct.forEach { el ->
+        println("${el.name}: ${listProduct.count { it == el }}")
+    }
+
+    println(listProduct)
+
+
+
+
+    /*println("Получение разницы между датами")
     val date = "2024-03-21 12:27:55.012529+00"
-    General.writeDurationData(date)
+    General.writeDurationData(date)*/
+
     /*println("Генератор паролей")
     print("Введите фразу: ")
     val phrase: String = readln()
     General.generatePassword(phrase)*/
-    print("Пароль: ")
+
+    /*print("Пароль: ")
     val pass = readln()
     println("Захэшированный: ")
     println("1." + General.getPassSHA512(pass))
-    println("2." + pass.toSHA512())
+    println("2." + pass.toSHA512())*/
+
+    //Хэшируем пароль
+    println(getSha512("pass123"))
+}
+
+fun getSha512(pass: String): String {
+    val md = MessageDigest.getInstance("SHA-512")
+    val mesdig = md.digest(pass.toByteArray())
+    val newStr = BigInteger(1, mesdig)
+    return String.format("%0128x", newStr)
 }
---
+++ src/ProductModel.kt
@@ -0,0 +1,7 @@
+data class ProductModel(
+    var isFavourit: Boolean? = false,
+    var name: String? = "",
+    var description: String? = "",
+    var price: Int? = 0,
+    var category: String = ""
+)
